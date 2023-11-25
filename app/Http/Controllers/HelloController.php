dba548f89dc18e0ccace5f8539b7ed5df089f90c
rendisibarani94 <rendisibarani94@gmail.com>
2023-11-25 15:45:20 +0700

    finish the template engine bug

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
     public function index(){
-        return view('layout.master');
+        return view('layout.bluePrint');
     }
 }
--- resources/views/layout/bluePrint.blade.php
+++ resources/views/layout/bluePrint.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Title</title>
+        <title>Judul</title>
         {{-- <link rel="stylesheet" href="{{ asset('css/uikit-rtl.css') }}">
         <link rel="stylesheet" href="{{ asset('css/uikit-rtl.min.css') }}">
         <link rel="stylesheet" href="{{ asset('css/uikit.css') }}">
